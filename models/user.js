f594c4f0465cb88efc6782b41f913e04e51c0db1
PARAS MEHTA <97521165+paras210@users.noreply.github.com>
2024-08-05 00:39:12 +0530

    mongodb

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost/NotesApp`)
+mongoose.connect(`mongodb://localhost:27017/NotesApp`)
 
 const userSchema = mongoose.Schema({
     username: String,
